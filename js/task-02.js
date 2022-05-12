fa44e62c855a2887936761541579d555c72a0f8c
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-05-12 16:35:23 +0100

    Solved task 2 (create a new item)

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const createItemCategoryOfIngredients = ingredients.map((el) => {
+  const item = document.createElement("li");
+  item.classList.add("item");
+  item.textContent = el;
+
+  return item;
+});
+
+const listIngredients = document.querySelector("#ingredients");
+listIngredients.append(...createItemCategoryOfIngredients);
