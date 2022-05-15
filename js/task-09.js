f9aae14e691686e9e1712a947bc378c353670ebf
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-05-15 13:57:58 +0100

    Solved task 9 (change color)

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+// в задании в LMS была прописана функция в конце с .padStart(6, 0) - этот код добавила в начальный файл
+
+const buttonChangeColor = document.querySelector(".change-color");
+const body = document.querySelector("body");
+let colorText = document.querySelector(".color");
+
+buttonChangeColor.addEventListener("click", onChangeButtonClick);
+
+function onChangeButtonClick(event) {
+  colorText.textContent = getRandomHexColor();
+  body.style.backgroundColor = colorText.textContent;
 }
