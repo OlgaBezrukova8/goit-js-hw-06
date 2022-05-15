55d8a47e22cd73f4432c532ca9acc1297fcebd10
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-05-15 13:37:41 +0100

    Solved task 8 (form)

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", onButtonLoginSubmit);
 
+function onButtonLoginSubmit(event) {
+  event.preventDefault();
+  let obj = {};
+
+  const formInputs = [...event.target.elements].filter(
+    (input) => input.tagName === "INPUT"
+  );
+
+  const isValidForm = formInputs.some((input) => input.value === "");
+
+  if (isValidForm) {
+    alert("All fields must be completed!");
+  }
+
+  formInputs.forEach((el) => (obj[el.getAttribute("type")] = el.value));
+  console.log(obj);
+
+  form.reset();
+}
